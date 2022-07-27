681d6c94a921a7eb0a0447e446e36edc50f62ea8
David Kelly <david.kelly@ticketswap.com>
2022-07-27 16:06:41 +0200

    adding s3 actions

--- test/visual.test.js
+++ test/visual.test.js
@@ -4,6 +4,7 @@ test('Google visual test', async ({page}) => {
 
     await test.step('When account page is loaded', async () => {
         await Promise.all([page.goto('http://www.google.com'), page.waitForNavigation({waitUntil: 'load'})])
+        await page.locator('input[title="Search"]').fill("test")
     })
 
     await test.step('Then the account page is visually correct', async () => {
