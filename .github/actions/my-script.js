0288f3232e7bbc50127aeb36f91576b7e515ea12
David Kelly <david.kelly@ticketswap.com>
2022-07-29 12:19:19 +0200

    wip

--- .github/actions/my-script.js
+++ .github/actions/my-script.js
@@ -2,15 +2,18 @@ const {Octokit} = require("@octokit/action");
 
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const runId = process.env.GITHUB_RUN_ID;
 
 async function run() {
 // See https://developer.github.com/v3/issues/#create-an-issue
-    const {data} = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    const { data } = await octokit.rest.actions.listJobsForWorkflowRun({
         owner,
         repo,
-        title: "My test issue",
+        runId,
     });
-    console.log("Issue created: %s", data.html_url);
+
+    const id = data.jobs.find(({ name }) => name === 'MY RUN NAME')?.id ?? undefined;
+    console.log(id);
 }
 
 run()
