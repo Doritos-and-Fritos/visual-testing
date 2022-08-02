2622dc1569a4feb1dbe6b508feac777b63875b62
David Kelly <david.kelly@ticketswap.com>
2022-08-02 16:17:08 +0200

    wip

---
+++ playwright.config.js
@@ -0,0 +1,20 @@
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+    testDir: 'test',
+    timeout: 30 * 1000,
+    snapshotDir: 'snapshots',
+    expect: {
+        timeout: 5000,
+    },
+    reporter: [['html', { open: 'never' }]],
+    use: {
+        actionTimeout: 0,
+        trace: 'retain-on-failure',
+        bypassCSP: true,
+    },
+}
+
+module.exports = config
